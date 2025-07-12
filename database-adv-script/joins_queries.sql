123de679a939bebf70e365a2cdcb22a75d183070
akellovj <akellohvictor@gmail.com>
2025-07-12 16:21:27 +0300

    join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -49,4 +49,25 @@ SELECT
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.user_id
 LEFT JOIN payments pay ON b.booking_id = pay.booking_id
-ORDER BY b.start_date DESC; 
+ORDER BY b.start_date DESC;
+
+-- FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.role,
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    CASE 
+        WHEN b.booking_id IS NULL THEN 'No bookings'
+        WHEN u.user_id IS NULL THEN 'Orphaned booking'
+        ELSE 'Valid booking'
+    END AS booking_status
+FROM users u
+FULL OUTER JOIN bookings b ON u.user_id = b.user_id
+ORDER BY u.user_id, b.start_date; 
